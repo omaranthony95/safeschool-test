d51c041376d7907ee4a71de681bf596120d6423f
omaranthony95 <cruzom95@gmail.com>
2025-08-08 13:12:51 -0500

    removed logs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,3 @@
-// const config = {
-//   plugins: ["@tailwindcss/postcss"],
-// };
-
-// export default config;
-
-/** @type {import('postcss-load-config').Config} */
 export default {
   plugins: {
     tailwindcss: {},
